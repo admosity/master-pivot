1e2eafa93c75f29260f7bb952f540df13515c5ea
Adam Lee <adam@ninjastack.com>
2015-11-08 03:05:39 -0500

    woejfjowejfowiejf:

--- server/lib/routes/api/receipt/_id/root.js
+++ server/lib/routes/api/receipt/_id/root.js
@@ -1,24 +1,38 @@
 var mongoose = require('mongoose');
-var router = require('express').Router({ mergeParams: true });
+var router = require('express').Router({
+  mergeParams: true
+});
 var nconf = require('nconf');
 var tesseract = require('lib/util/tesseract');
 var Receipt = mongoose.model('Receipt');
-
+var LineItem = mongoose.model('LineItem');
 
 
 router.route('/')
 
-  .get(function (req, res) {
-    Receipt.findById(req.params.id).populate('lineItems').lean().exec(function (err, receipt) {
-      if(err) return res.error(500, '181b62ca-6a75-4b61-af9e-99390399f64f');
+.get(function(req, res) {
+    Receipt.findById(req.params.id).populate('lineItems').lean().exec(function(err, receipt) {
+      if (err) return res.error(500, '181b62ca-6a75-4b61-af9e-99390399f64f');
 
       return res.ok(receipt);
     });
   })
-  .put(function (req, res) {
+  .put(function(req, res) {
+    var receipt = req.body;
     // update line item owners
-    Receipt.findById(req.params.id).exec(function (err, receipt) {
-
+    Receipt.findById(req.params.id).exec(function(err, receipt) {
+      if(err) return res.error(500, '7ddfe7f7-e889-4873-8abf-cefaf8d3d17c');
+      async.each(receipt.lineItems, function(lineItem, cb) {
+        LineItem.findByIdAndUpdate(lineItem._id, {
+          $set: {
+            owner: lineItem.owner
+          }
+        }).lean().exec(cb);
+      }, function(err) {
+        if (err) {
+          return res.error(500, 'ebd2f5c0-75e7-458f-bcfb-8237ad600ea9');
+        }
+      })
     });
   })
 
