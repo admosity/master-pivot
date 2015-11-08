52c72968045252d00d27de65995ef8dd33595d0c
Adam Lee <adam@ninjastack.com>
2015-11-08 09:17:26 -0500

    PARSE

--- server/lib/models/LineItem.js
+++ server/lib/models/LineItem.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var LineItemSchema = new Schema({
-  text: {type: Number, required: true },
+  text: {type: String },
+  value: { type: Number, required: true},
   paid: { type: Boolean, default: false },
   owner: { type: Schema.Types.ObjectId, ref: 'User' },
   receipt: { type: Schema.Types.ObjectId, ref: 'Receipt' },
--- server/lib/routes/api/receipt/root.js
+++ server/lib/routes/api/receipt/root.js
@@ -6,6 +6,8 @@ var fs = require('fs');
 var shortid = require('shortid');
 var path = require('path');
 var Receipt = mongoose.model('Receipt');
+var LineItem = mongoose.model('LineItem');
+var async = require('async');
 
 
 console.log(path.resolve(global.rootDir + '/uploads/' + 'loolo' + '.jpg'));
@@ -28,6 +30,7 @@ router.route('/')
         var lines = result.split("\n");
         var subtotal;
         var total;
+        var lineItemsToSave = [];
         for (var i = 0; i < lines.length; i++) {
           var l = lines[i];
 
@@ -35,32 +38,52 @@ router.route('/')
             continue;
           }
 
+          console.log('WHAT THE FUCKKKKK');
+          console.log(l);
+
           var match;
 
-          if(match = /(\D+)*.*?(\d+)\s*.\s*(\d+)$/i.exec(l)) {
-            newReceipt.lineItems.push(new LineItem({
-              text: match[1],
+          if(match = /(\D+)*.{0,2}?(\d+)\s{0,2}.\s{0,2}(\d{0,2})$/i.exec(l)) {
+            if(!match[1]) continue;
+            var newLineItem = new LineItem({
+              text: match[1].trim(),
               value: parseFloat(match[2] + '.' + match[3]),
               receipt: newReceipt,
-            }))
+            });
+            lineItemsToSave.push(newLineItem);
+            newReceipt.lineItems.push(newLineItem);
             continue;
           }
 
-          if(subtotal != null && (match = /total.*?(\d+)\s*.\s*(\d+)$/i.exec(l))) {
+          if(subtotal != null && (match = /total.{0,2}?(\d+)\s{0,2}.\s{0,2}(\d{0,2})$/i.exec(l))) {
             total = parseFloat(match[1] + '.' + match[2]);
             break;
           }
 
-          if(match = /subtotal.*?(\d+)\s*.\s*(\d+)$/i.exec(l)) {
+          if(match = /subtotal.{0,2}?(\d+)\s{0,2}.\s{0,2}(\d{0,2})$/i.exec(l)) {
             subtotal = parseFloat(match[1] + '.' + match[2]);
             continue;
           }
         }
 
-        newReceipt.save(function (err) {
-          if(err) return res.error(500, '5783a631-70b9-4f34-b9a1-40b696717450');
-          return res.ok(newReceipt);
-        });
+        async.each(lineItemsToSave, function (lineItem, cb) {
+          lineItem.save(cb);
+        }, function (err) {
+          console.log(err);
+          if(err) return res.error(500, 'e45707a8-47e4-4c88-862e-ab6acfe209f8');
+          newReceipt.save(function (err) {
+            if(err) return res.error(500, '5783a631-70b9-4f34-b9a1-40b696717450');
+            newReceipt.populate('lineItems', function (err, receipt) {
+              return res.ok(receipt);
+            })
+          });
+        })
+
+
+      })
+      .catch(function (err) {
+        console.log(err);
+        return res.error(500, '96dadda8-f5e0-434a-ae44-a1b7a004fa86');
       })
 
     });
